3c0d4bfbc4dd2b0384b6abc7cd676b512279661f
ghazalnzhr <96284770+ghazalnzhr@users.noreply.github.com>
2022-01-04 17:17:55 +0330

    account for button state

--- 14.ino
+++ 14.ino
@@ -1,17 +1,30 @@
+/* By pushing the buttons once and then letting go of it,
+the buzzer will make noise until the melody
+is finished, then it'll automatically stop
+if we don't pick up our finger, the alarm will keep on repeating
+ */
+
+const int buttonPin = 2;
 const int buzzerPin = 8;
 
+// Sounds 
 int melody[] = {500, 800, 500, 800, 500, 800, 500, 800, 
 700, 700}; //  frequencies
 int noteDurations[] = {300, 300, 300, 300, 300, 300, 300, 300,
 800, 800};
 int noteDuration;
 
-void setup() {}
+// Keep track of state
+int buttonState;
+int prevButtonState;
 
+void setup() {
+  pinMode(buttonPin, INPUT);
+}
 void loop() {
-  // iterate over the notes of the melody:
-
-  for (int thisNote = 0; thisNote < 10; thisNote++) {
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH) {
+    for (int thisNote = 0; thisNote < 10; thisNote++) {
     
     noteDuration = noteDurations[thisNote];
     // args: pin, freq, dur
@@ -25,8 +38,11 @@ void loop() {
 
     delay(pauseBetweenNotes);
 
-    // stop the tone playing:
-    noTone(8);
-
-} 
+    prevButtonState = LOW;
+  } 
+  } else if (buttonState != prevButtonState) {
+        // stop the tone playing:
+      noTone(8);
+  } 
+  
 }
